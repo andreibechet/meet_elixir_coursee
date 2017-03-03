fca6a3917784ec27d6e0df61422fa6cd98e78cd6
Andrei Bechet <andreibechet@Andreis-Work-MacBook-Pro.local>
2017-03-03 13:38:49 +0100

    reading the list of m23u8 files and their corresponding program_id and bandwidth from the index file

--- lib/streamers.ex
+++ lib/streamers.ex
@@ -3,6 +3,9 @@ defmodule Streamers do
   Documentation for Streamers.
   """
 
+  require Record
+  Record.defrecord :m3u8, [program_id: nil, path: nil, bandwidth: nil]
+
   @doc """
   Find streaming index file in the given directory.
 
@@ -13,13 +16,43 @@ defmodule Streamers do
   def find_index(directory) do
     files = Path.join(directory, "*.m3u8")
     if file = Enum.find(Path.wildcard(files), fn(file) -> is_index?(file) end) do # can also be written is_index?(&1)
-      Path.basename(file)
+      file
     end
   end
 
   def is_index?(file) do
     File.open!(file,
       fn(pid) -> IO.read(pid, 25) == "#EXTM3U\n#EXT-X-STREAM-INF" end)
+  end
+
+  def extract_m3u8(index_file) do
+    []
+    File.open!(index_file,
+      fn(pid) ->
+        IO.read(pid, :line)
+        do_extract_m3u8(pid, Path.dirname(index_file), [])
+      end)
+  end
+
+  defp do_extract_m3u8(pid, dir, acc) do
+    case IO.read(pid, :line) do
+      :eof -> Enum.reverse(acc)
+      stream_inf ->
+        path = IO.read(pid, :line)
+        do_extract_m3u8(pid, dir, stream_inf, path, acc)
+    end
+  end
+
+  defp do_extract_m3u8(pid, dir, stream_inf, path, acc) do
+    # expecting #EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=110000
+    << "#EXT-X-STREAM-INF:PROGRAM-ID=", program_id, ",BANDWIDTH=", bandwidth :: binary >> = stream_inf
+    # program_id is 1 char; the bandwidth is marked as binary since we don't know how many bits are left
+
+    path = Path.join(dir, path |> String.strip)
+    bandwidth = bandwidth |> String.strip |> String.to_integer
+    record = m3u8(program_id: program_id - ?0, path: path, bandwidth: bandwidth )
+    # do the - ?0 in order to convert it from ascii to actual int value (ascii start at 48)
+    do_extract_m3u8(pid, dir, [record | acc])
   end
 
 end
--- test/streamers_test.exs
+++ test/streamers_test.exs
@@ -2,12 +2,25 @@ defmodule StreamersTest do
   use ExUnit.Case, async: true
 
   doctest Streamers
+  #alias Streamers.M3U8, as: M3U8 # you can leave this part out as it takes the last member by default
+
+  @index_file "test/fixtures/emberjs/9af0270acb795f9dcafb5c51b1907628.m3u8"
 
   test "find index file in a directory" do
-    assert Streamers.find_index("test/fixtures/emberjs") == "9af0270acb795f9dcafb5c51b1907628.m3u8"
+    assert Streamers.find_index("test/fixtures/emberjs") == @index_file
   end
 
   test "returns nil for not available" do
     assert Streamers.find_index("test/fixtures/not_available") == nil
   end
+
+  test "extracts m3u8 from index file" do
+    m3u8s = Streamers.extract_m3u8(@index_file)
+    IO.puts(m3u8s)
+    assert Enum.at(m3u8s, 0) == Streamers.m3u8(program_id: 1,
+                                    bandwidth: 110000,
+                                    path: "test/fixtures/emberjs/8bda35243c7c0a7fc69ebe1383c6464c.m3u8")
+
+  end
+
 end
